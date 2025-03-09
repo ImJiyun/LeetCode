318280aa316d366b4f3e5e6daa9159f5ec4b4b68
Jiyun <162008947+ImJiyun@users.noreply.github.com>
2025-03-09 10:35:00 +0900

    Time: 0 ms (100%), Space: 54.5 MB (19.98%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -7,6 +7,6 @@ const searchRange = function(nums, target) {
     let first = nums.findIndex(num => num === target);
     let last = nums.findLastIndex(num => num === target);
 
-    if (!first || !last) return [-1, -1];
+    if (first === -1 || last === -1) return [-1, -1];
     return [first, last];
 };
