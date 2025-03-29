978b8c80fc749908d9c3424a0fc5a758a553e0a6
Jiyun <162008947+ImJiyun@users.noreply.github.com>
2025-03-29 22:16:50 +0900

    Time: 1 ms (86.31%), Space: 57.5 MB (23.57%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+const canPlaceFlowers = function(flowerbed, n) {
+    for (let i = 0; i < flowerbed.length; i++) {
+       if (flowerbed[i] === 0) {
+        let prev = (i === 0) ? 0 : flowerbed[i-1];
+        let next = (i === flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+        if (prev === 0 && next === 0) {
+            flowerbed[i] = 1;
+            n--;
+
+            if (n === 0) return true;
+        }
+       }
+    }
+    return n <= 0;
+};
