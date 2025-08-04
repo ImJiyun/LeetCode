b5ee156be6ecdb23d436295dcb2fbc2cf9063303
Jiyun <162008947+ImJiyun@users.noreply.github.com>
2025-08-04 09:50:47 +0900

    Time: 0 ms (100%), Space: 12.5 MB (14.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,18 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
         N = len(nums)
+        if N == 1:
+            return nums[0]
+        if N == 2:
+            return max(nums[0], nums[1])
+
         dp = [0] * N
-        if N >= 2:
-            dp[0], dp[1] = nums[0], nums[1]
-        elif N >= 1:
-            dp[0] = nums[0]
-        else:
-            return []        
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
-        for i in range(N):
-            for j in range(i+2, N):
-                print(i, j)
-                dp[j] = max(dp[j], dp[i]+nums[j])
+        for i in range(2, N):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
 
-        return max(dp[N-1], dp[N-2])        
+        return dp[-1]                     
