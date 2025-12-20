5ee64f2b9e8bf43ff545b7351895d728f466c270
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2025-12-20 10:32:35 +0900

    Time: 272 ms (80.62%), Space: 0B (100%) - LeetHub

---
+++ 3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+WITH ranked AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY student_id, subject ORDER BY exam_date) AS rn_asc,
+        ROW_NUMBER() OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS rn_desc
+    FROM
+        Scores
+)
+
+SELECT 
+    student_id,
+    subject,
+    MAX(CASE WHEN rn_asc = 1 THEN score END) AS first_score,
+    MAX(CASE WHEN rn_desc = 1 THEN score END) AS latest_score
+FROM
+    ranked
+GROUP BY
+    student_id, 
+    subject
+HAVING
+    first_score < latest_score
