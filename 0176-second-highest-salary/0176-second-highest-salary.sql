1ef50c69cc3cff0333240691224ebaefb621128b
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2025-12-26 14:02:13 +0900

    Time: 193 ms (88.57%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,24 @@
-# Write your MySQL query statement below
-SELECT (
-    IFNULL((SELECT DISTINCT salary
-            FROM Employee
-            ORDER BY salary DESC
-            LIMIT 1 OFFSET 1), NULL)
-) AS SecondHighestSalary
+-- Write your PostgreSQL query statement below
+WITH rank_table AS(
+    SELECT
+        *,
+        DENSE_RANK() OVER(ORDER BY salary DESC) AS salary_rank
+    FROM 
+        Employee
+)
+
+SELECT
+    MAX(salary) AS SecondHighestSalary
+FROM
+    rank_table
+WHERE
+    salary_rank = 2
+
+-- SELECT
+--     DISTINCT salary
+-- FROM
+--     Employee
+-- ORDER BY 
+--     salary DESC
+-- LIMIT 
+--     2
