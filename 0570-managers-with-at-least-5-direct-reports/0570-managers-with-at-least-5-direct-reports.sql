fa72599b3c5acf84b9da936045ed312a0efd42c2
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2025-12-30 19:14:04 +0900

    Time: 301 ms (49.66%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,26 @@
-# Write your MySQL query statement below
-SELECT e1.name
-FROM Employee e1
-JOIN Employee e2
-ON e1.id = e2.managerId
-GROUP BY e1.id
-HAVING COUNT(*) >= 5
+-- Write your PostgreSQL query statement below
+WITH five_reports AS (
+    SELECT
+        e1.id
+    FROM
+        Employee AS e1
+    LEFT JOIN 
+        Employee AS e2
+        ON e1.id = e2.managerid
+    GROUP BY
+        e1.id
+    HAVING 
+        COUNT(*) >= 5
+)
+
+SELECT
+    name
+FROM
+    Employee
+WHERE
+    id IN (
+        SELECT
+            *
+        FROM
+            five_reports
+    )
