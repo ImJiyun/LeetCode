0b3d7304236e3527a5608bf476a423a4444e56f3
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-01-09 15:00:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,13 +1,52 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
+-- 첫 시도 
+-- WITH rn_table AS (
+--     SELECT
+--         *,
+--         ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY year) AS rn
+--     FROM
+--         Sales 
+-- )
+
+-- SELECT
+--     product_id,
+--     year AS first_year,
+--     quantity,
+--     price
+-- FROM
+--     rn_table
+-- WHERE
+--     rn = 1
+
+
+-- 두 번째 시도
+
+
+WITH rn_table AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY year) AS rn
+    FROM (
+        SELECT
+        product_id,
+        year,
+        SUM(quantity) AS quantity,
+        price 
+    FROM
+        Sales
+    GROUP BY
+        product_id,
+        year,
+        price 
+    ) 
+)
+
 SELECT
     product_id,
     year AS first_year,
     quantity,
     price
-FROM 
-    Sales
-WHERE (product_id, year) IN (
-    SELECT product_id, MIN(year)
-    FROM Sales
-    GROUP BY product_id
-)        
+FROM
+    rn_table
+WHERE
+    rn = 1
