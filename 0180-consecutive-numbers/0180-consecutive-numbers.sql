7d4a1c12b19cbd179ea7c52df71f8241f8746a13
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-01-15 19:57:26 +0900

    Time: 490 ms (85.34%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,10 +1,39 @@
 # Write your MySQL query statement below
-SELECT DISTINCT num AS ConsecutiveNums
-FROM (
-    SELECT 
-        num,
-        LAG(num, 1) OVER (ORDER BY id) AS prev1,
-        LAG(num, 2) OVER (ORDER BY id) AS prev2 
-    FROM Logs
-) AS t
-WHERE num = prev1 AND num = prev2
+# 처음 시도 : 10/23 testcase passed
+-- WITH lag_lead_table AS (
+--     SELECT
+--         *,
+--         LAG(num, 2) OVER (ORDER BY id) AS prev_num,
+--         LEAD(num, 2) OVER (ORDER BY id) AS next_num
+--     FROM 
+--         Logs
+-- )
+
+-- SELECT
+--     num AS ConsecutiveNums 
+-- FROM
+--     lag_lead_table
+-- WHERE
+--     num = prev_num 
+--     AND
+--     prev_num = next_num  
+
+WITH lag_lead_table AS (
+    SELECT
+        *,
+        LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+        LAG(num, 2) OVER (ORDER BY id) AS prev_prev_num
+    FROM 
+        Logs
+)
+
+# DISTINCT 안 썼더니 19 / 23 testcases passed
+
+SELECT
+    DISTINCT num AS ConsecutiveNums 
+FROM
+    lag_lead_table
+WHERE
+    num = prev_num 
+    AND
+    prev_num = prev_prev_num  
