6efe10a282ad1b3ce56c17e011258c33916a1ce2
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-01-16 15:36:53 +0900

    Time: 291 ms (60.26%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,35 +1,71 @@
 # Write your MySQL query statement below
-WITH union_table AS (
-    SELECT
-        *
-    FROM
-        RequestAccepted
-    UNION ALL
-    SELECT
-        accepter_id,
-        requester_id,
-        accept_date 
-    FROM
-        RequestAccepted
-), friend_cnts AS (
-    SELECT
-        requester_id AS id,
-        COUNT(requester_id) AS num
-    FROM
-        union_table
-    GROUP BY
-        requester_id 
-)
+-- WITH union_table AS (
+--     SELECT
+--         *
+--     FROM
+--         RequestAccepted
+--     UNION ALL
+--     SELECT
+--         accepter_id,
+--         requester_id,
+--         accept_date 
+--     FROM
+--         RequestAccepted
+-- ), friend_cnts AS (
+--     SELECT
+--         requester_id AS id,
+--         COUNT(requester_id) AS num
+--     FROM
+--         union_table
+--     GROUP BY
+--         requester_id 
+-- )
 
-SELECT
-    id,
-    num
+-- SELECT
+--     id,
+--     num
+-- FROM (
+--     SELECT
+--         *,
+--         RANK() OVER (ORDER BY num DESC) AS rn
+--     FROM
+--         friend_cnts
+-- ) AS t
+-- WHERE
+--     rn = 1
+
+
+-- WITH BASE AS(
+--     SELECT
+--         requester_id,
+--         COUNT(requester_id) as cnt_r
+       
+--     FROM RequestAccepted
+--     GROUP BY
+--         requester_id
+        
+-- )
+
+-- WITH BASE_2 AS (
+--     SELECT
+--         accepter_id,
+--         count(accepter_id) as cnt_a
+--     from
+
+-- SELECT
+-- *
+-- FROM BASE
+
+SELECT 
+    base.id,
+    (
+        (SELECT COUNT(*) FROM RequestAccepted WHERE requester_id = base.id) +
+        (SELECT COUNT(*) FROM RequestAccepted WHERE accepter_id = base.id)
+) AS num
 FROM (
-    SELECT
-        *,
-        RANK() OVER (ORDER BY num DESC) AS rn
-    FROM
-        friend_cnts
-) AS t
-WHERE
-    rn = 1
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION
+    SELECT accepter_id AS id FROM RequestAccepted
+) AS base
+ORDER BY num DESC
+LIMIT 1;
