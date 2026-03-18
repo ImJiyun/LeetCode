60e423a19b2045e30563c35256c0e5426623bb4c
Jiyun Kim <162008947+ImJiyun@users.noreply.github.com>
2026-03-18 14:48:04 +0900

    Time: 317 ms (52.87%), Space: 0B (100%) - LeetHub

---
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -0,0 +1,45 @@
+# Write your MySQL query statement below
+WITH t1 AS (
+    SELECT
+        customer_id, 
+        COUNT(*) AS order_cnt,
+        SUM(CASE WHEN transaction_type = 'refund' THEN 1 END) / COUNT(*) AS refund_rate
+    FROM
+        customer_transactions
+    GROUP BY
+        customer_id
+    HAVING
+        order_cnt >= 3 
+        AND
+        (refund_rate IS NULL OR refund_rate < 0.2)
+), rn_date AS(
+    SELECT
+        *,
+        RANK() OVER (PARTITION BY customer_id ORDER BY transaction_date) AS first_date,
+        RANK() OVER (PARTITION BY customer_id ORDER BY transaction_date DESC) AS last_date
+    FROM
+        customer_transactions
+), t2 AS (
+    SELECT
+        customer_id,
+        DATEDIFF(MAX(CASE WHEN last_date = 1 THEN transaction_date END), MAX(CASE WHEN first_date = 1 THEN transaction_date END)) AS active_days
+    FROM
+        rn_date
+    WHERE
+        first_date = 1 OR last_date = 1
+    GROUP BY
+        customer_id
+    HAVING
+        active_days >= 30
+)
+
+SELECT
+    t1.customer_id
+FROM
+    t1
+JOIN
+    t2
+ON
+    t1.customer_id = t2.customer_id
+ORDER BY
+    customer_id
